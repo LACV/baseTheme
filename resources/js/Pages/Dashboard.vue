ab531050dae2aacd9679e7ece55462a6ddba7193
LACVCruz <malucruz1101@gmail.com>
2024-04-19 08:37:40 -0500

    update in Dashboard.vue
    New update is made to the Dashboard.vue component

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -10,7 +10,7 @@ import Spreadsheet  from '@/Components/Spreadsheet.vue';
 
 <template>
   <AppLayout title="Dashboard">
-    <div class="flex flex-wrap justify-start gap-16">
+    <div class="flex flex-wrap justify-start gap-14">
       <Spreadsheet class=""
         title="titulo" 
         footer="top 1" 
@@ -32,6 +32,45 @@ import Spreadsheet  from '@/Components/Spreadsheet.vue';
         footer="top 1" 
         Icon="anchor"
         colorcontent="g">
+        The sun slowly dipped below the horizon, casting a warm golden glow across the tranquil ocean. Seagulls glided gracefully through the air, their calls echoing in the distance. As the day faded into dusk, a sense of peace enveloped the coastal town, offering a moment of serenity amidst the chaos of the world.
+      </Spreadsheet>
+
+      <Spreadsheet class=""
+        title="titulo" 
+        footer="top 1" 
+        Icon="anchor"
+        colorcontent="w">
+        The sun slowly dipped below the horizon, casting a warm golden glow across the tranquil ocean. Seagulls glided gracefully through the air, their calls echoing in the distance. As the day faded into dusk, a sense of peace enveloped the coastal town, offering a moment of serenity amidst the chaos of the world.
+      </Spreadsheet>
+      <Spreadsheet class=""
+        title="titulo" 
+        footer="top 1" 
+        Icon="menu"
+        colorcontent="i">
+        The sun slowly dipped below the horizon, casting a warm golden glow across the tranquil ocean. Seagulls glided gracefully through the air, their calls echoing in the distance. As the day faded into dusk, a sense of peace enveloped the coastal town, offering a moment of serenity amidst the chaos of the world.
+      </Spreadsheet>
+      
+      <Spreadsheet class=""
+        title="titulo" 
+        footer="top 1" 
+        Icon="anchor"
+        colorcontent="e">
+        The sun slowly dipped below the horizon, casting a warm golden glow across the tranquil ocean. Seagulls glided gracefully through the air, their calls echoing in the distance. As the day faded into dusk, a sense of peace enveloped the coastal town, offering a moment of serenity amidst the chaos of the world.
+      </Spreadsheet>
+      
+      <Spreadsheet class=""
+        title="titulo" 
+        footer="top 1" 
+        Icon="anchor"
+        colorcontent="su">
+        The sun slowly dipped below the horizon, casting a warm golden glow across the tranquil ocean. Seagulls glided gracefully through the air, their calls echoing in the distance. As the day faded into dusk, a sense of peace enveloped the coastal town, offering a moment of serenity amidst the chaos of the world.
+      </Spreadsheet>
+
+      <Spreadsheet class=""
+        title="titulo" 
+        footer="top 1" 
+        Icon="anchor"
+        colorcontent="se">
         The sun slowly dipped below the horizon, casting a warm golden glow across the tranquil ocean. Seagulls glided gracefully through the air, their calls echoing in the distance. As the day faded into dusk, a sense of peace enveloped the coastal town, offering a moment of serenity amidst the chaos of the world.
       </Spreadsheet>
     </div>
