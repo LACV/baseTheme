8bd643e15a7a3cf07fc1c41c702f4597e65b2b29
ibeth15 <146088296+2006-15@users.noreply.github.com>
2024-05-16 11:50:21 -0500

    changes to icons in tags from span to svg
    changes to icons in tags from span to svg

--- resources/js/Components/ContainerNotification.vue
+++ resources/js/Components/ContainerNotification.vue
@@ -21,8 +21,16 @@
 <template>
     <div class="notification  mr-4" :class="estadoNotificationClass">
          <div class="icon mr-2" >
-             <span class="material-icons-sharp" v-if="EstadoNofication">done</span>
-             <span class="material-icons-sharp" v-else>done_all</span>
+             <svg class="" v-if="EstadoNofication" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+             <path d="m4 15.6 3.055-3.056A4.913 4.913 0 0 1 7 12.012a5.006 5.006 0 0 1 5-5c.178.009.356.027.532.054l1.744-1.744A8.973 8.973 0 0 0 12 5.012c-5.388 0-10 5.336-10 7A6.49 6.49 0 0 0 4 15.6Z"/>
+             <path d="m14.7 10.726 4.995-5.007A.998.998 0 0 0 18.99 4a1 1 0 0 0-.71.305l-4.995 5.007a2.98 2.98 0 0 0-.588-.21l-.035-.01a2.981 2.981 0 0 0-3.584 3.583c0 .012.008.022.01.033.05.204.12.402.211.59l-4.995 4.983a1 1 0 1 0 1.414 1.414l4.995-4.983c.189.091.386.162.59.211.011 0 .021.007.033.01a2.982 2.982 0 0 0 3.584-3.584c0-.012-.008-.023-.011-.035a3.05 3.05 0 0 0-.21-.588Z"/>
+             <path d="m19.821 8.605-2.857 2.857a4.952 4.952 0 0 1-5.514 5.514l-1.785 1.785c.767.166 1.55.25 2.335.251 6.453 0 10-5.258 10-7 0-1.166-1.637-2.874-2.179-3.407Z"/>
+             </svg>
+
+             <svg class="" v-else aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+             <path fill-rule="evenodd" d="M4.998 7.78C6.729 6.345 9.198 5 12 5c2.802 0 5.27 1.345 7.002 2.78a12.713 12.713 0 0 1 2.096 2.183c.253.344.465.682.618.997.14.286.284.658.284 1.04s-.145.754-.284 1.04a6.6 6.6 0 0 1-.618.997 12.712 12.712 0 0 1-2.096 2.183C17.271 17.655 14.802 19 12 19c-2.802 0-5.27-1.345-7.002-2.78a12.712 12.712 0 0 1-2.096-2.183 6.6 6.6 0 0 1-.618-.997C2.144 12.754 2 12.382 2 12s.145-.754.284-1.04c.153-.315.365-.653.618-.997A12.714 12.714 0 0 1 4.998 7.78ZM12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd"/>
+             </svg>
+
          </div>
          <div class="content"> <!-- Aplicar clase de opacidad si estadoNotification es false -->
              <div class="ps-2">
@@ -30,8 +38,14 @@
                  <div class="text-xs text-gray-400 dark:text-gray-500">a few moments ago</div>
              </div>
          </div>
-         <span class="material-icons-sharp ml-2 text-4xl text-grey-300 dark:text-gray-500 hover:text-primary">clear_all</span>
-     </div>
+    
+         <svg class="" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-width="1.1" d="M5 7h14M5 12h14M5 17h10"/>
+        </svg>
+
+
+     
+        </div>
  </template>
  
  
--- resources/js/Layouts/base/Notification.vue
+++ resources/js/Layouts/base/Notification.vue
@@ -24,8 +24,8 @@ const estadoClass = computed(()=>{
 
 
 const datos = [
-  { id: 1, type: true },    
-  { id: 2, type: false },
+  { id: 1, type: false },    
+  { id: 2, type: true },
   
  
  
@@ -36,8 +36,14 @@ const datos = [
 <template>
   <div class="containerNoti" v-if="$page.props.auth.user" :class="estadoClass">
     <div @click="toggleNotification()" id="Notification" data-dropdown-toggle="Notification" class="cursor-pointer hover:text-primary dark:hover:text-primary dark:text-gray-500">
-      <span class="material-icons-sharp" v-if="Estado">chat_bubble</span>
-      <span class="material-icons-sharp" v-else>mark_chat_unread</span>
+      <svg class="" v-if="Estado" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M17.133 12.632v-1.8a5.406 5.406 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V3.1a1 1 0 0 0-2 0v2.364a.955.955 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C6.867 15.018 5 15.614 5 16.807 5 17.4 5 18 5.538 18h12.924C19 18 19 17.4 19 16.807c0-1.193-1.867-1.789-1.867-4.175ZM6 6a1 1 0 0 1-.707-.293l-1-1a1 1 0 0 1 1.414-1.414l1 1A1 1 0 0 1 6 6Zm-2 4H3a1 1 0 0 1 0-2h1a1 1 0 1 1 0 2Zm14-4a1 1 0 0 1-.707-1.707l1-1a1 1 0 1 1 1.414 1.414l-1 1A1 1 0 0 1 18 6Zm3 4h-1a1 1 0 1 1 0-2h1a1 1 0 1 1 0 2ZM8.823 19a3.453 3.453 0 0 0 6.354 0H8.823Z"/>
+      </svg>
+
+      <svg class="" v-else aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M17.133 12.632v-1.8a5.406 5.406 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V3.1a1 1 0 0 0-2 0v2.364a.955.955 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C6.867 15.018 5 15.614 5 16.807 5 17.4 5 18 5.538 18h12.924C19 18 19 17.4 19 16.807c0-1.193-1.867-1.789-1.867-4.175ZM8.823 19a3.453 3.453 0 0 0 6.354 0H8.823Z"/>
+      </svg>
+
     </div>
     <div v-if="iOpen" id="Notification" class="top-10 absolute bg-white rounded-lg shadow max-w-screen-sm dark:bg-gray-200" style="max-height: 300px; overflow-y: auto;" @mouseleave="toggleNotification(false)">
       <div class="block px-4 py-2 text-center cursor-pointer text-primary dark:text-primary">Notifications</div>
@@ -46,7 +52,11 @@ const datos = [
       </div>
       <a href="#" class="block px-4 py-2 text-center text-gray-600 hover:text-primary dark:hover:text-primary dark:text-gray-500">
         <div class="inline-flex items-center">
-          <span class="material-icons-sharp text-lg mr-1">visibility</span>
+          <svg class="text-lg mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.1" d="m8 7 4 4 4-4m-8 6 4 4 4-4"/>
+          </svg>
+
+
           View all
         </div>
       </a>
